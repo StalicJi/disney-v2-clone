23d47e159bb42425d6bbfdc3c3fe0d5b2ec33337
StalicJi <a7330831@gmail.com>
2023-12-23 01:37:56 +0800

    20231223 final

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+
 type Props = {
   params: {
     id: string;
@@ -7,10 +10,19 @@ type Props = {
   };
 };
 
-const GenrePage = ({ params: { id }, searchParams: { genre } }: Props) => {
+const GenrePage = async ({
+  params: { id },
+  searchParams: { genre },
+}: Props) => {
+  const movies = await getDiscoverMovies(id);
+
   return (
-    <div>
-      Welcome to the genre page with ID: {id} and name: {genre}
+    <div className="max-w-7xl mx-auto">
+      {/* Azure OPENAI service Suggestion */}
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Resoults for {genre}</h1>
+      </div>
+      <MoviesCarousel title={`Genre`} movies={movies} isVertical />
     </div>
   );
 };
--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getPopularMovies, getSearchMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -6,16 +8,27 @@ type Props = {
   };
 };
 
-const SearchPage = ({ params: { term } }: Props) => {
+const SearchPage = async ({ params: { term } }: Props) => {
   //   404notFound
   if (!term) notFound();
 
   const termToUse = decodeURI(term);
 
   //API call to get the Searched Movies
-  //API call to get the Popular Movies
+  const movie = await getSearchMovies(termToUse);
 
-  return <div>Welcome to the search page: {termToUse}</div>;
+  //API call to get the Popular Movies
+  const popularMovie = await getPopularMovies();
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+        <MoviesCarousel title="Movies" movies={movie} isVertical />
+        <MoviesCarousel title="You may also like" movies={popularMovie} />
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
